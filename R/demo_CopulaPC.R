6bb95e5aafce2b2a01cd3c23966e32e68ce24ac6
crf.dqq <crf.dqq@gmail.com>
2018-03-27 05:19:10 +0200

    Added a demo to show how the Copula PC algorithm works for data with missing values.

--- R/demo_CopulaPC.R
+++ R/demo_CopulaPC.R
@@ -1,17 +1,20 @@
 #############################################################################################
-# Goal: This is a demo to show how the Rank PC algorithm works for incomplete data.
+# Goal: This is a demo to show how the Copula PC algorithm works for incomplete data.
 #############################################################################################
 
 #### 0. Dependent Packages and Parameters ####
 
 ## dependencies
 require(pcalg)
+require(sbgcop)
+require(infotheo)
 source('R/gaussCItestLocal.R')
 source('R/inferCopulaModel.R')
+source('R/addMAR.R')
 
 ## parameters
 # No. of variables
-p <- 15
+p <- 10
 # sample size
 n <- 1000
 # significance level
@@ -25,40 +28,45 @@ g <- randomDAG(p, 2/(p-1))
 var.names <- g@nodes
 ## simulate Gaussian data given the DAG
 Z <- rmvDAG(n, g)
-## nonparanormal transformation
-# here we assume the observed distribution of each margin is chi-squared with df;
-# one could change it to the desired distribution.
-YY <- apply(Z, 2, function(x) qchisq(pnorm(x), df = sample(2:10, 1)))
+## discretize some variables into ordinal
+# index of discrete variables
+iy <- sample(1:p, round(p/2))
+# YY, mixed complete data
+YY <- Z
+YY[,iy] <- matrix(unlist(apply(Z[,iy], 2, discretize, nbins = 2)), nrow = n, byrow = F)
 ## add missing values
 # the expected percentage of missing values
 beta <- 0.25
 # please choose MCAR or MAR
+# Y, mixed incomplete data
 # MCAR
 Y <- apply(YY, 2, function(x){x[sample(n, round(n*runif(1,0,2*beta)))] = NA; x})
 # MAR
-Y <- addMAR(YY, beta)
-
+ZZ <- addMAR(Z, beta)
+Y <- YY
+Y[is.na(ZZ)] <- NA
 
 #### 2. Causal Discovery ####
 
 ## estimate underlying correlation matrix and (effective) sample size
-# No. of missing values in each margin
-count.NA <- apply(Y, 2, function(x){ sum(is.na(x))})
-# rank correlations (here, transformed Kendall's tau. one could use others.) 
-corr.rank <- sin(pi/2 * cor(Y, use = 'pairwise.complete.obs', method = 'kendall'))
-# local effective sample size (LESS)
-less.rank <- (1-count.NA/n) %*% t(1-count.NA/n) * n
-# global effective sample size (GESS)
-gess.rank <- mean(less.rank[upper.tri(less.rank)])
+# copula object
+cop.obj <- inferCopulaModel(Y, nsamp = 1000, S0 = diag(p)/n, verb = T)
+# correlation matrix samples
+C_samples <- cop.obj$C.psamp[,, 501:1000]
+# average correlation matrix
+corr.cop <- apply(C_samples, c(1,2), mean)
+# local effective sample size
+less.cop <- ((1-corr.cop^2)^2)/apply(C_samples,c(1,2), var)
+# global effective sample size
+gess.cop <- mean(less.cop[upper.tri(less.cop)])
 
-## call the standard PC algorithm for causal discovery
-# RPC (rank PC) + SS (original sample size)
-graph.rpc.ss <- pc(suffStat = list(C = corr.rank, n = n), 
+## call the PC algorithm for causal discovery
+# CoPC + SS
+graph.cpc.ss <- pc(suffStat = list(C = corr.cop, n = n), 
                    indepTest = gaussCItest, labels = var.names, alpha = alpha, conservative = T)
-# RPC + GESS
-graph.rpc.gess <- pc(suffStat = list(C = corr.rank, n = gess.rank), 
+# CoPC + GESS
+graph.cpc.gess <- pc(suffStat = list(C = corr.cop, n = gess.cop), 
                      indepTest = gaussCItest, labels = var.names, alpha = alpha, conservative = T)
-# RPC + LESS
-graph.rpc.less <- pc(suffStat = list(C = corr.rank, n = n, ESS.Mat = less.rank), 
+# CoPC + LESS
+graph.cpc.less <- pc(suffStat = list(C = corr.cop, n = n, ESS.Mat = less.cop), 
                      indepTest = gaussCItestLocal, labels = var.names, alpha = alpha, conservative = T)
-
